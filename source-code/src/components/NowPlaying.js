e39e2e2ddde6c404b67188934dc9861d17b23b8d
gordonnrat <gordonnrat@gmail.com>
2023-08-11 22:40:27 -0700

    fixed bug with volume bar when mute toggled, icons change with volume changes; added previous and next track buttons; changed styling of bar to keep things static as name of song info changes

--- source-code/src/components/NowPlaying.js
+++ source-code/src/components/NowPlaying.js
@@ -13,11 +13,12 @@ function NowPlaying() {
         },
         {
             name: 'Cello Suite No. 1',
-            artist: 'Bach',
+            artist: 'Johann Sebastian Bach',
             src: require("./music/Bach-Cello-Suite-No.-1-G-Major.mp3"),
             img: 'https://images.unsplash.com/photo-1506423456648-c11ccb27338d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80'
         }
     ]
+
     const audioRef = useRef(null);
     const timelineRef = useRef(null);
     const volumeRef = useRef(null);
@@ -25,10 +26,12 @@ function NowPlaying() {
     const [isMuted, setIsMuted] = useState(false);
     const [timelinePercentage, setTimelinePercentage] = useState(0);
     const [volumeValue, setVolumeValue] = useState(0.5);
-
+    const [currentTrack, setCurrentTrack] = useState(0);
+    const [hasInteracted, setHasInteracted] = useState(false);
+  
     function toggleAudio() {
         const audio = audioRef.current;
-
+        setHasInteracted(true);
         if (isPlaying) {
             audio.pause();
         } else {
@@ -36,20 +39,32 @@ function NowPlaying() {
         }
         setIsPlaying(!isPlaying);
     }
+    
+    const nextSong = () => {
+        setCurrentTrack((currentTrack + 1) % musicInfo.length);
+        setTimelinePercentage(0);
+    };
+
+    const prevSong = () => {
+        setCurrentTrack((currentTrack - 1 + musicInfo.length) % musicInfo.length);
+        setTimelinePercentage(0);
+    };
 
     useEffect(() => {
-        const audio = audioRef.current;
-        const onEnded = () => {
-            setIsPlaying(false);
+        if (hasInteracted && isPlaying) {
+          const audio = audioRef.current;
+
+          audio.pause();
+          audio.src = "";
+  
+          audio.src = musicInfo[currentTrack].src;
+
+          audio.play().catch(error => {
+            console.error(error);
+          });;
         }
-
-        audio.addEventListener("ended", onEnded);
-
-        return () => {
-            audio.removeEventListener("ended", onEnded);
-        }
-    });
-
+    }, [hasInteracted, currentTrack]);
+    
     useEffect(() => {
         const audio = audioRef.current;
 
@@ -58,34 +73,44 @@ function NowPlaying() {
             setTimelinePercentage(percentagePosition);
         };
 
+        function songEnded() {
+            nextSong();
+        }
+
         audio.ontimeupdate = changeTimelinePosition;
-    
+        audio.onended = songEnded;
     });
 
     const timelineChange = (e) => {
-        const newPosition = e.target.value;
+        const position = e.target.value;
         const audio = audioRef.current;
-        const newTime = (newPosition / 100) * audio.duration;
-        audio.currentTime = newTime;
-        setTimelinePercentage(newPosition);
+        const time = (position / 100) * audio.duration;
+        audio.currentTime = time;
+        setTimelinePercentage(position);
     };
 
     const volumeChange = (e) => {
-        const newVolume = e.target.value;
+        const volume = e.target.value;
         const audio = audioRef.current;
-        audio.volume = newVolume;
-        setVolumeValue(newVolume);
+        audio.volume = volume;
+        setVolumeValue(volume);
+        if (audio.volume === 0) {
+            setIsMuted(true);
+            console.log("its 0");
+          } else {
+            setIsMuted(false);
+          }
     };
 
     const toggleSound = () => {
         const audio = audioRef.current;
+        audio.volume = volumeValue;
         if (isMuted) {
           audio.volume = volumeValue; 
         } else {
           setVolumeValue(audio.volume); 
           audio.volume = 0;
         }
-        audio.muted = !isMuted;
         setIsMuted(!isMuted);
       };
 
@@ -100,20 +125,28 @@ function NowPlaying() {
     return(
         <div className="audio-player now-playing-hide">
 
-            <div className="controls">
-                <div className="audio-player-info">
-                    <img src={musicInfo[0].img} alt="" />
-                    <div className="audio-player-info-title">
-                        <p className="audio-player-info-song-title">{musicInfo[0].name}</p>
-                        <p className="audio-player-info-song-artist">{musicInfo[0].artist}</p>
+            <div className="audio-bar">
+                <div className="audio-player-song-info">
+                    <img src={musicInfo[currentTrack].img} alt="" />
+                    <div className="audio-player-info">
+                        <p className="audio-player-info-title">{musicInfo[currentTrack].name}</p>
+                        <p className="audio-player-info-artist">{musicInfo[currentTrack].artist}</p>
                     </div>
                 </div>
 
-                <div className="audio-play-timeline">
-                    <button className="player-button" onClick={toggleAudio}>
-                        {isPlaying ? <i className="fa-solid fa-pause fa-xl"></i> : <i className="fa-solid fa-play fa-xl	"></i>}
-                    </button>
-                    <input style={timelineProgress} type="range" className="timeline" max="100" value={timelinePercentage} onChange={timelineChange}ref={timelineRef} />
+                <div className="controls">
+                    <div className="audio-play-timeline">
+                        <button className="player-button" onClick={toggleAudio}>
+                            {isPlaying ? <i className="fa-solid fa-pause fa-xl"></i> : <i className="fa-solid fa-play fa-xl	"></i>}
+                        </button>
+                        <button className="control-button" onClick={prevSong}>
+                            <i className="fa-solid fa-backward-step fa-xl"></i> 
+                        </button>
+                        <input style={timelineProgress} type="range" className="timeline" max="100" value={timelinePercentage} onChange={timelineChange} ref={timelineRef} />
+                        <button className="control-button" onClick={nextSong}>
+                            <i className="fa-solid fa-forward-step fa-xl"></i> 
+                        </button>
+                    </div>
                 </div>
 
                 <div className="audio-volume">
@@ -122,10 +155,9 @@ function NowPlaying() {
                     </button>
                     <input style={volumeProgress} type="range" className="volume-control" max="1" step="0.01" value={isMuted ? 0 : volumeValue} onChange={volumeChange} ref={volumeRef} />
                 </div>
-
             </div>
 
-            <audio ref={audioRef} src={musicInfo[0].src} />
+            <audio ref={audioRef} src={musicInfo[currentTrack].src} />
 
         </div>
     )
