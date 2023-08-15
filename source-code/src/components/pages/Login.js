8c3c0b4b2716a0273fdd7ec4f531d1e0755cd058
gordonnrat <gordonnrat@gmail.com>
2023-08-14 20:46:14 -0700

    changed css to be more specific to the sign up page to not mess up other pages, added styling to get rid of play bar on login and sign up pages

--- source-code/src/components/pages/Login.js
+++ source-code/src/components/pages/Login.js
@@ -2,37 +2,47 @@
 import React, { useState } from 'react'; 
 import { useNavigate } from 'react-router-dom'; 
 import '../Login.css'; 
+
 const Login = () => { 
-const [email, setEmail] = useState(''); 
-const [password, setPassword] = useState(''); 
-const navigate = useNavigate(); 
-const handleLogin = () => { 
-// Simulate authentication logic for demonstration 
-if (email === 'user@gmail.com' && password === 'password') { 
-navigate('/home'); 
-} else { 
-alert('Invalid credentials'); 
-} 
+    const [email, setEmail] = useState(''); 
+    const [password, setPassword] = useState(''); 
+    const navigate = useNavigate(); 
+    const handleLogin = () => { 
+    // Simulate authentication logic for demonstration 
+    if (email === 'user@gmail.com' && password === 'password') { 
+        navigate('/home'); 
+       } else { 
+        alert('Invalid credentials'); 
+        } 
+        }; 
+    return ( 
+        <div className="login-container"> 
+            <h2 className="login-heading">Log In to Soundex</h2> 
+            <input 
+                className="login-input" 
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+            /> 
+            <input 
+                className="login-input" 
+                type="password" 
+                placeholder="Password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                /> 
+            <button className="login-button" onClick={handleLogin}>Login</button> <p className="signup-link"> <button onClick={() => navigate('/signup')}>Don’t have an account?</button></p> 
+        
+            <style>{`
+            .now-playing-hide{
+              display:none;
+            }
+            `}
+            </style>
+        
+        </div>
+    ); 
 }; 
-return ( 
-<div className="login-container"> 
-<h2 className="login-heading">Log In to Soundex</h2> 
-<input 
-className="login-input" 
-type="email" 
-placeholder="Email" 
-value={email} 
-onChange={(e) => setEmail(e.target.value)} 
-/> 
-<input 
-className="login-input" 
-type="password" 
-placeholder="Password" 
-value={password} 
-onChange={(e) => setPassword(e.target.value)} 
-/> 
-<button className="login-button" onClick={handleLogin}>Login</button> <p className="signup-link"> <button onClick={() => navigate('/signup')}>Don’t have an account?</button></p> 
-</div>
-); 
-}; 
+
 export default Login; 
--- source-code/src/components/pages/SignUp.js
+++ source-code/src/components/pages/SignUp.js
@@ -2,57 +2,69 @@
 import React, { useState } from 'react'; 
 import { useNavigate } from 'react-router-dom'; 
 import '../SignUp.css'; 
+
 const SignUp = () => { 
-const [firstName, setFirstName] = useState(''); 
-const [lastName, setLastName] = useState(''); 
-const [email, setEmail] = useState(''); 
-const [dateOfBirth, setDateOfBirth] = useState(''); 
-const [username, setUsername] = useState(''); 
-const [password, setPassword] = useState(''); 
-const navigate = useNavigate(); 
-const handleSignUp = () => { 
+    const [firstName, setFirstName] = useState(''); 
+    const [lastName, setLastName] = useState(''); 
+    const [email, setEmail] = useState(''); 
+    const [dateOfBirth, setDateOfBirth] = useState(''); 
+    const [username, setUsername] = useState(''); 
+    const [password, setPassword] = useState(''); 
+    const navigate = useNavigate(); 
+
+    const handleSignUp = () => { 
 // Simulate registration logic for demonstration 
 // You can replace this with your actual registration process 
-if (firstName && lastName && email && dateOfBirth && username && password) { alert('Registration successful'); 
-navigate('/login'); 
-} else { 
-alert('Please provide valid information');
-} 
+    if (firstName && lastName && email && dateOfBirth && username && password) { alert('Registration successful'); 
+    navigate('/login'); 
+    } else { 
+    alert('Please provide valid information');
+    } 
+    }; 
+    return ( 
+        <div className='signup-page'> 
+            <h2>Sign Up</h2> 
+            <input 
+                type="text" 
+                placeholder="First Name" 
+                value={firstName} 
+                onChange={(e) => setFirstName(e.target.value)} /> 
+            <input 
+                type="text" 
+                placeholder="Last Name" 
+                value={lastName} 
+                onChange={(e) => setLastName(e.target.value)} /> 
+            <input 
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} /> 
+            <input 
+                type="date" 
+                placeholder="Date of Birth" 
+                value={dateOfBirth} 
+                onChange={(e) => setDateOfBirth(e.target.value)} /> 
+            <input 
+                type="text" 
+                placeholder="Username" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} /> 
+            <input 
+                type="password" 
+                placeholder="Password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleSignUp}>Sign Up</button> 
+
+            <style>{`
+            .now-playing-hide{
+              display:none;
+            }
+            `}
+            </style>
+
+        </div> 
+    ); 
 }; 
-return ( 
-<div> 
-<h2>Sign Up</h2> 
-<input 
-type="text" 
-placeholder="First Name" 
-value={firstName} 
-onChange={(e) => setFirstName(e.target.value)} /> 
-<input 
-type="text" 
-placeholder="Last Name" 
-value={lastName} 
-onChange={(e) => setLastName(e.target.value)} /> 
-<input 
-type="email" 
-placeholder="Email" 
-value={email} 
-onChange={(e) => setEmail(e.target.value)} /> 
-<input 
-type="date" 
-placeholder="Date of Birth" 
-value={dateOfBirth} 
-onChange={(e) => setDateOfBirth(e.target.value)} /> 
-<input 
-type="text" 
-placeholder="Username" 
-value={username} 
-onChange={(e) => setUsername(e.target.value)} /> 
-<input 
-type="password" 
-placeholder="Password" 
-value={password} 
-onChange={(e) => setPassword(e.target.value)} />
-<button onClick={handleSignUp}>Sign Up</button> </div> 
-); 
-}; 
+
 export default SignUp;
